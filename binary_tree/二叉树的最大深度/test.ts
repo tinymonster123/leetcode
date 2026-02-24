c8c3bb1b238b53d7294e74b28896a1f70ef06ea8
tinymonster123 <lfhxgs123@gmail.com>
2026-02-24 15:59:19 +0800

    chore: 重写二叉树最大深度

---
+++ binary_tree/二叉树的最大深度/test.ts
@@ -0,0 +1,31 @@
+import { TreeNode } from '../treeNode'
+
+// function maxDepth(root: TreeNode | null): number {
+//     let count = 0
+//     if(!root) return count
+//     count += 1
+//     const leftDepth = maxDepth(root.left)
+//     const rightDepth = maxDepth(root.right)
+//     return Math.max(leftDepth,rightDepth)
+// };
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) return 0
+    const leftDepth = maxDepth(root.left)
+    const rightDepth = maxDepth(root.right)
+    return Math.max(leftDepth, rightDepth) + 1
+};
