38204223d453374abb7a4e7adeaa61cb7a4e87d7
tinymonster123 <lfhxgs123@gmail.com>
2026-04-26 12:36:29 +0800

    feat: 完成杨辉三角

---
+++ dp/杨辉三角/index.ts
@@ -0,0 +1,14 @@
+function generate(numRows: number): number[][] {
+    const dp: number[][] = Array(numRows + 1)
+    for (let i = 1; i <= numRows; i++) {
+        dp[i] = Array(i).fill(1)
+    }
+    dp[1][0] = 1
+    for (let i = 2; i <= numRows; i++) {
+        for (let j = 0; j <= i - 1; j++) {
+            dp[i][j] = (dp[i - 1][j] ?? 0) + (dp[i - 1][j - 1] ?? 0)
+        }
+    }
+    dp.shift()
+    return dp
+};
