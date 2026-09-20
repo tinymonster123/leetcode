4cafab1f9f74bd3a99f1c21e219a768fcbf5e0a2
tinymonster123 <lfhxgs123@gmail.com>
2026-09-20 15:08:36 +0800

    feat: 最长公共子序列

---
+++ multi_dp/最长公共子序列/index.ts
@@ -0,0 +1,17 @@
+function longestCommonSubsequence(text1: string, text2: string): number {
+    const len1: number = text1.length
+    const len2: number = text2.length
+    const dp: number[][] = Array.from({ length: len1 + 1 }, () => Array(len2 + 1).fill(0))
+
+    for (let i = 1; i <= len1; i++) {
+        for (let j = 1; j <= len2; j++) {
+            if (text1[i - 1] === text2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+
+    return dp[len1][len2]
+};
