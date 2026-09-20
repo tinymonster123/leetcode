9807e742b8bf18c113afce1920269594a808fe8b
tinymonster123 <lfhxgs123@gmail.com>
2026-09-20 12:50:23 +0800

    feat: 最小路径和

---
+++ multi_dp/最小路径和/index.ts
@@ -0,0 +1,22 @@
+function minPathSum(grid: number[][]): number {
+    const m: number = grid.length
+    const n: number = grid[0].length
+
+    const dp: number[][] = Array.from({ length: m }, () => Array(n).fill(0))
+
+    for (let i = 0; i < m; i++) {
+        dp[i][0] = (dp[i - 1] ? dp[i - 1][0] : 0) + grid[i][0]
+    }
+
+    for (let j = 1; j < n; j++) {
+        dp[0][j] = (dp[0][j - 1] ?? 0) + grid[0][j]
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+        }
+    }
+
+    return dp[m - 1][n - 1]
+};
